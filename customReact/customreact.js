347de551549871523129edb6f2844ba26edf3b75
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-02-13 19:16:24 +0000

    new code

---
+++ customReact/customreact.js
@@ -0,0 +1,20 @@
+function customRender(reactElement , container)
+{
+    const  domElement = document.createElement(reactElement.type)
+    domElement.innerHtml = reactElement.children
+    domElement.setAttribute('href' , reactElement.props.href)
+    domElement.setAttribute('target' , reactElement.props.target )
+
+    mainContainer.appendChild(domElement)
+}
+const reactElement = {
+    type : 'a',
+    props : {
+        href : 'google.com',
+        target : '_blank'
+    },
+    children : 'Click me to visit google'
+}
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement , mainContainer)
