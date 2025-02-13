24dccdecb5c7f4ca7f837993874390ebe663253c
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-02-13 20:27:19 +0000

    new code

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,10 +2,11 @@
 import Chai from "./chai"
 function App() {
   // return ke andar ap sirf ek hi element return kar skta ho 
+    const username  = "chai aur code "
   return (
     <>
    <Chai/>
-   <h1> Hanjo kaisi lagi chai</h1>
+   <h1> Hanjo kaisi lagi chai {username} </h1>
    <h4>bye bye bye bye </h4>
    </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,25 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+///
+function MyApp()
+{
+  return(
+    <div>
+      <h1> hello chai pi lo</h1>
+    </div>
+  )
+}
+
+const anotheruser  = "bhanu"
+const element = (
+  <a href="https://google.com" target = '_blank'> Visited google</a>
+)
+
+const element2 = React.createElement(
+  'a',{href : 'http://google.com'}, 'click me to visit google',anotheruser
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App/>
 )
