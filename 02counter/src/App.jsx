da8e6c5c32fdb8167df96859b3c8429f9924ced4
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-02-14 19:10:05 +0000

    more about hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let  [counter , setCounter ] = useState(15)
+
+  //let counter = 5
+  let removeVal = ()=>
+  {
+    if(counter > 0)
+    setCounter(counter-1)
+  }
+  const addValue = ()=>
+  {
+    console.log("value added" ,  counter);
+    //counter = counter + 1
+    if(counter < 20)
+    setCounter(counter +1)
+  }
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>counter value : {counter} </h2>
+
+    <button
+    onClick={addValue}>
+      add value {counter}</button>
+    <br></br>
+    <button
+    onClick={removeVal}
+    >remove value {counter}</button>
+    <p>footer :  {counter}</p>
+    </>
+  )
+}
+
+export default App
