9f40722a1aa33e932100b4573ca4ac6590f809fe
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-02-21 20:25:30 +0000

    project

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Layout from './Layout'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='p-4  bg-green-600'> React router</h1>
+     <Layout/>
     </>
   )
 }
