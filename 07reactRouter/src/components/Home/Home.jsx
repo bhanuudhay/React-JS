1c0d92fdd5f145f0670cf402cfffcf65ac342a37
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-02-21 20:29:59 +0000

    router

--- 07reactRouter/src/components/Home/Home.jsx
+++ 07reactRouter/src/components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import "tailwindcss";
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
@@ -45,4 +44,3 @@ export default function Home() {
         </div>
     );
 }
-
