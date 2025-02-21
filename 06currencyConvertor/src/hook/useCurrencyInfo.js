aef7620e172471cf7581590fbf6221a3fb04b692
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-02-21 03:39:07 +0000

    new commit

---
+++ 06currencyConvertor/src/hook/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import {useEffect , useState} from "react";
+
+function useCurrencyInfo(currency)
+{
+    const [data,setData] = useState({})
+    useEffect(()=>{
+        try{
+        fetch(`https://open.er-api.com/v6/latest/${currency}`)
+        .then( (res)=> res.json())
+        .then((res)=> setData(res[currency]))
+        console.log(data);
+        }
+        catch(error)
+        {
+            console.log(error);
+        }
+    } ,  [currency])
+    
+}
