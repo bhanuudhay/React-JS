07ab7692a52c1dad8f9c6ddce6ee06490328961c
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-02-22 06:43:41 +0000

    reactRouter

--- 07reactRouter/src/components/Footer/Footer.jsx
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link , NavLink } from 'react-router-dom';
 
 export default function Footer() {
     return (
@@ -20,14 +20,19 @@ export default function Footer() {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
-                                    <Link to="/" className="hover:underline">
+                                    <NavLink 
+                                    to="/" 
+                                    className={({isActive})=> `hover:underline ${isActive ? "text-orange-700" : "text-gray-700" } `
+                                    }>
                                         Home
-                                    </Link>
+                                    </NavLink>
                                 </li>
                                 <li>
-                                    <Link to="/about" className="hover:underline">
+                                    <NavLink 
+                                    to="/about" 
+                                    className={({isActive})=> `hover:underline ${isActive ? "text-orange-700" : "text-gray-700" }` }>
                                         About
-                                    </Link>
+                                    </NavLink>
                                 </li>
                             </ul>
                         </div>
@@ -36,7 +41,7 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
@@ -72,7 +77,7 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
+                        <a href="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png" className="hover:underline">
                             hiteshchoudhary
                         </a>
                         . All Rights Reserved.
